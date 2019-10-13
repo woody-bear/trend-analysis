62b4ac73150030f80c8d24ffcafb67be07978a2d
BaekGeunYoung <dvmflstm@naver.com>
2019-10-13 23:39:36 +0900

    table element에 unique key props 추가

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -13,17 +13,17 @@ const DataTable = ({data} : Props) => {
             <thead>
                 <tr>
                 {
-                    data.columns.map(column => <th>{column.name}</th>)
+                    data.columns.map((column, idx) => <th key={idx}>{column.name}</th>)
                 }
                 </tr>
             </thead>
             <tbody>
             {
-                data.rows.map(row =>
-                    <tr>
+                data.rows.map((row, idx) =>
+                    <tr key={idx}>
                         {
-                            row.map((rowItem : any) =>
-                            <td>
+                            row.map((rowItem : any, idx : number) =>
+                            <td key={idx}>
                                 {rowItem}
                             </td>
                             )
